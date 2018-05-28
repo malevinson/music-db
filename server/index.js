5cda56ac955f95b9640e40439c64688112ef3380
Matt Levinson <mlevinso@organic.com>
2018-05-27 18:56:57 -0700

    attempt to refresh spotify access token before every request

--- server/index.js
+++ server/index.js
@@ -29,6 +29,16 @@ spotifyApi.clientCredentialsGrant().then(
 
 function getArtistImage(artistQuery) {
     return new Promise(function(resolve, reject) {
+        spotifyApi.refreshAccessToken().then(
+            function(data) {
+                console.log('The access token has been refreshed!');
+                // Save the access token so that it's used in future calls
+                spotifyApi.setAccessToken(data.body['access_token']);
+            },
+            function(err) {
+                console.log('Could not refresh access token', err);
+            }
+        );
         spotifyApi.searchArtists(artistQuery).then(
             function(data) {
                 if (
